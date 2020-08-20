cdc527e9d3b8a5fde5f90276d9854e82ff4af5b9
vitorrrocha <vitorrochaflorip@hotmail.com>
2020-08-20 06:00:08 -0300

    Secret_key now is hidden

--- controle_gastos/settings.py
+++ controle_gastos/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from secrets_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l_c&)kw7gq1s938moayj!d)4#9q%uiqo2$5_)v8qh7u8@ii7=0'
+SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
